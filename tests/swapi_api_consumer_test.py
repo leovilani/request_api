1a0ef91e42cd46891ad285adae82a27c2a87f3e4
Leonardo <leonardovselan@gmail.com>
2022-09-26 23:07:06 -0300

    feat(swapi_api_consumer): First interaction

---
+++ tests/swapi_api_consumer_test.py
@@ -0,0 +1,7 @@
+from src.infra.swapi_api_consumer import SwapiApiConsumer
+
+def test_get_starships():
+    swapi_api_consumer = SwapiApiConsumer()
+    response = swapi_api_consumer.get_starships(page=1)
+
+    print(response)
